6fac297bc9dc7aedd03d0462fbd247252bee09b3
Guillaume Giudicelli <guillaume.giudicelli@inl.gov>
2022-04-04 16:24:35 -0600

    Add SSF routine to sideUOs, for functors, refs #18086

--- framework/include/userobject/SideUserObject.h
+++ framework/include/userobject/SideUserObject.h
@@ -48,4 +48,19 @@ protected:
   const Real & _current_side_volume;
 
   const BoundaryID & _current_boundary_id;
+
+  /**
+   * Determine the single sided face argument when evaluating a functor on a face.
+   * This is used to perform evaluations of material properties with the actual face values of
+   * their dependences, rather than interpolate the material property to the boundary.
+   * @param fi the FaceInfo for this face
+   * @param limiter_type the limiter type, to be specified if more than the default average
+   *        interpolation is required for the parameters of the functor
+   * @param correct_skewness whether to perform skew correction at the face
+   */
+  Moose::SingleSidedFaceArg singleSidedFaceArg(
+      const MooseFunctorName & functor_name,
+      const FaceInfo & fi,
+      Moose::FV::LimiterType limiter_type = Moose::FV::LimiterType::CentralDifference,
+      bool correct_skewness = false) const;
 };
