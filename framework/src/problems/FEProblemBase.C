05a33b9fb996a2dc8189d76e0e6ea4e2100b70f5
tanome <mauriciotano@gmail.com>
2025-01-17 09:48:30 -0700

    first working linear k epsilon model Refs #29846

--- framework/src/problems/FEProblemBase.C
+++ framework/src/problems/FEProblemBase.C
@@ -7499,6 +7499,9 @@ FEProblemBase::computeLinearSystemTags(const NumericVector<Number> & soln,
     _functions.jacobianSetup(tid);
   }
 
+  _current_linear_sys->compute(EXEC_NONLINEAR);
+
+ /*
   try
   {
     computeSystems(EXEC_NONLINEAR);
@@ -7515,6 +7518,7 @@ FEProblemBase::computeLinearSystemTags(const NumericVector<Number> & soln,
     // other errors or unhandled exceptions being thrown.
     return;
   }
+  */
 
   computeUserObjects(EXEC_NONLINEAR, Moose::POST_AUX);
   executeControls(EXEC_NONLINEAR);
